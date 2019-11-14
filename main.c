2d4166293308da7153a8a51f2ae23777024919fd
= <=>
2019-11-14 11:39:39 +0000

    fin

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-  struct stat* st = malloc(sizeof(struct stat));
-  stat("demo.txt",st);
-  printf("size of demo.txt: %d Bytes\n",st->st_size);
-  printf("permissions of demo.txt: %d\n",st->st_mode);
-  time_t* atime = &(st->st_atime);
-  time_t* mtime = &(st->st_mtime);
-  printf("last accessed of demo.txt: %s\n",asctime(atime));
-  printf("last modified of demo.txt: %s\n",asctime(mtime));
-  free(st);
+  struct stat st;
+  stat("demo.txt",&st);
+  printf("size of demo.txt: %d Bytes\n",st.st_size);
+  printf("permissions of demo.txt: %d\n",st.st_mode);
+  time_t* atime = &(st.st_atime);
+  time_t* mtime = &(st.st_mtime);
+  printf("last accessed of demo.txt: %s\n",localtime(atime));
+  printf("last modified of demo.txt: %s\n", localtime(mtime));
   return 0;
 }

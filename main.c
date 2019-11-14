dfddf0fc478d706359060b4ce324bcfa8cab3343
Kevin Cai <k1029384756c@gmail.com>
2019-11-14 06:26:40 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <time.h>
 int main(){
   struct stat* st = malloc(sizeof(struct stat));
   stat("demo.txt",st);
   printf("size of demo.txt: %d Bytes\n",st->st_size);
   printf("permissions of demo.txt: %d\n",st->st_mode);
-  printf("last accessed of demo.txt: %d\n",ctime(st->st_atime));
-  printf("last modified of demo.txt: %d\n",ctime(st->st_mtime));
+  time_t* atime = &(st->st_atime);
+  time_t* mtime = &(st->st_mtime);
+  printf("last accessed of demo.txt: %s\n",asctime(atime));
+  printf("last modified of demo.txt: %s\n",asctime(mtime));
   free(st);
   return 0;
 }
